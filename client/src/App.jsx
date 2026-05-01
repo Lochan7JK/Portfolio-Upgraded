9065a26d79ee6aacbe4eaae92990234e8429abb0
Lochan <110106642+Lochan7JK@users.noreply.github.com>
2026-05-01 12:49:20 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,24 +7,98 @@ import "./index.css";
 import "@fontsource/poppins";
 import "@fontsource/inter";
 
+import { useEffect, useState } from "react";
+import Loader from "./components/Loader";
+import { AnimatePresence, motion } from "framer-motion";
+
 function App() {
-  return(
+  const [loading, setLoading] = useState(false);
+  const [loaderText, setLoaderText] = useState("");
+  const [firstVisit, setFirstVisit] = useState(false);
+
+  useEffect(() => {
+    const visited = sessionStorage.getItem("visited");
+
+    if (!visited) {
+      // FIRST VISIT
+      sessionStorage.setItem("visited", "true");
+
+      setFirstVisit(true);
+
+      // show welcome message for 2 sec
+      setTimeout(() => {
+        setFirstVisit(false);
+      }, 2000);
+
+    } else {
+      // REVISIT
+      setLoading(true);
+
+      const messages = [
+        "Connecting...",
+        "Welcome back 👀",
+        "Take a deeeeep breath",
+        "Booting again...",
+        "Summoning Lochan...",
+      ];
+
+      setLoaderText(messages[Math.floor(Math.random() * messages.length)]);
+
+      setTimeout(() => {
+        setLoading(false);
+      }, 800);
+    }
+  }, []);
+
+
+  return (
     <>
-      {/* Your App */}
-      <Home />
+      <AnimatePresence mode="wait">
+        {firstVisit ? (
+
+          <motion.div
+            key="welcome"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            className="fixed inset-0 z-[999] bg-[#191919] flex items-center justify-center"
+          >
+            <h1 className="text-white text-xl md:text-4xl tracking-widest text-center">
+              <span className="text-[#00ADB5]">Welcome creature</span>, <br />
+              mucho gusto 👀
+            </h1>
+          </motion.div>
+
+
+        ) : loading ? (
+          <Loader key="loader" text={loaderText} />
+        ) : (
+          <motion.div
+            key="home"
+            initial={{ opacity: 0, scale: 0.98 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 1, ease: "easeOut" }}
+          >
+            {/* Your App */}
+            <Home />
+          </motion.div>
+        )}
+      </AnimatePresence>
+
 
       <ToastContainer 
-        position="top-right"
-        autoClose={3000}
-        theme="dark"
+          position="top-right"
+          autoClose={3000}
+          theme="dark"
 
-        hideProgressBar={false}
-        newestOnTop
-        closeOnClick
-        pauseOnHover
-        draggable
-      />
-    </>
+          hideProgressBar={false}
+          newestOnTop
+          closeOnClick
+          pauseOnHover
+          draggable
+        />
+
+      </>
   );
 }
 
